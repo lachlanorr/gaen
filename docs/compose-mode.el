8fb5bfd3660150ec026c0b58a1fff60ab6970634
Lachlan Orr <lorr@gaen.org>
2022-05-19 03:45:11 -0500

    Corrected curly brace indent behavior of emacs compose-mode

--- docs/compose-mode.el
+++ docs/compose-mode.el
@@ -2,7 +2,7 @@
 ;; compose-mode.el - Compose language major mode for Emacs
 ;;
 ;; Gaen Concurrency Engine - http:;;gaen.org
-;; Copyright (c) 2014-2019 Lachlan Orr
+;; Copyright (c) 2014-2022 Lachlan Orr
 ;;
 ;; This software is provided 'as-is', without any express or implied
 ;; warranty. In no event will the authors be held liable for any damages
@@ -44,11 +44,6 @@
 
 ;; Syntax highlighting using keywords
 (defconst compose-font-lock-keywords-1
-  ;; Candiates to add to keywords
-  ;;  update
-  ;;  ready
-  ;;  post
-
   (list
    '("\\(#[a-zA-Z_][a-zA-Z0-9_]+\\)" . font-lock-constant-face)
    '("\\<\\(as\\|b\\(?:ool\\|reak\\|yte\\)\\|c\\(?:ase\\|har\\|o\\(?:lor\\|mponents?\\|nst\\)\\)\\|d\\(?:efault\\|o\\(?:uble\\)?\\)\\|e\\(?:lse\\|ntity\\)\\|f\\(?:alse\\|loat\\|or\\)\\|ha\\(?:lf\\|ndle\\)\\|i\\(?:f\\|nt\\)\\|long\\|mat\\(?:43\\|[43]\\)\\|none\\|return\\|s\\(?:hort\\|tring\\|witch\\)\\|t\\(?:his\\|r\\(?:ansform\\|ue\\)\\)\\|u\\(?:int\\|long\\|s\\(?:hort\\|ing\\)\\)\\|v\\(?:ec[234]\\|oid\\)\\|while\\|asset\\)\\>" . font-lock-keyword-face)
@@ -60,6 +55,13 @@
   "Default highlighting expressions for Compose mode")
 
 
+(defun compose-prev-line()
+  (interactive)
+  (beginning-of-line)
+  (skip-chars-backward "\n\t ")
+  (beginning-of-line)
+  (skip-chars-forward "\n\t "))
+
 ;; Indentation
 (defun compose-indent-line()
   "Indent current line as Compose code"
@@ -68,31 +70,27 @@
 
   (if (bobp)
       (indent-line-to 0) ; First line is never indented
-    (let ((not-indented t) cur-indent)
-      (if (looking-at "^[ \t]*}") ; closing '}', decrease the indention
-          (progn
+    (let (cur-indent)
+      (if (looking-at-p "^[ \t]*}") ; closing '}', decrease the indention
+          (save-excursion
+            (compose-prev-line)
+            (if (looking-at-p "{")
+                (setq cur-indent (current-indentation))
+              (setq cur-indent (max 0 (- (current-indentation) 4)))))
+        (if (looking-at-p "^[ \t]*{")
             (save-excursion
-              (forward-line -1)
-              (setq cur-indent (- (current-indentation) default-tab-width)))
-            (if (< cur-indent 0) ; Don't indent past left margin
-                (setq cur-indent 0)))
-        (save-excursion
-          (while not-indented
-            (forward-line -1)
-            (if (looking-at "^[ \t]*}") ; previous line is '}', indent at its level
-                (progn
-                  (setq cur-indent (current-indentation))
-                  (setq not-indented nil))
-              (if (looking-at "^[ \t]*{") ; previous line is '{', indent a level beyond it
-                  (progn
-                    (setq cur-indent (+ (current-indentation) default-tab-width))
-                    (setq not-indented nil))
-                (if (bobp) ; First line is never indented
-                    (setq not-indented nil)))))))
+              (compose-prev-line)
+              (if (looking-at-p "{")
+                  (setq cur-indent (+ (current-indentation) 4))
+                (setq cur-indent (current-indentation))))
+          (save-excursion
+            (compose-prev-line)
+            (if (looking-at-p "{")
+                (setq cur-indent (+ (current-indentation) 4))
+              (setq cur-indent (current-indentation))))))
       (if cur-indent
           (indent-line-to cur-indent) ; do the indentation...
         (indent-line-to 0)))))        ; ... or if no hint, remove indentation
-
 
 ;; The syntax table
 (defvar compose-mode-syntax-table
